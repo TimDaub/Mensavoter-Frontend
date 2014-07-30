a58d80b14be94ad21eca31a1470764029eefc28b
Linkeex <tim.daubenschuetz@uni-ulm.de>
2014-07-30 20:42:52 +0200

    adding scores

--- app/scripts/services/DayFactory.js
+++ app/scripts/services/DayFactory.js
@@ -1,10 +1,26 @@
 'use strict';
 
 angular.module('mensavoterFrontendApp')
-  .factory('DayFactory', function (Restangular) {
+  .factory('DayFactory', function (Restangular, $q) {
     return {
       getToday: function () {
-        return Restangular.one('days', 'today').doGET();
+        var deferred = $q.defer();
+
+        Restangular.one('days', 'today').doGET()
+          .then(function(res) {
+
+            // Sort by voting
+            for(var i = 0; i < res.Mensa.meals.length; i++) {
+              res.Mensa.meals[i].score = res.Mensa.meals[i].upvotes-res.Mensa.meals[i].downvotes;
+            }
+
+            deferred.resolve(res);
+
+          }, function(err) {
+            deferred.reject(err);
+          });
+
+        return deferred.promise;
       }
     };
   });
