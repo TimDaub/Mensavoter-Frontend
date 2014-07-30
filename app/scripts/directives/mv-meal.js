c9114521520720b3a491c33d2f83162d4fdc30db
linkeex <tim.daubenschuetz@gmail.com>
2014-07-30 21:56:00 +0200

    Adding Feedback

--- app/scripts/directives/mv-meal.js
+++ app/scripts/directives/mv-meal.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('mensavoterFrontendApp')
-  .directive('mvMeal', function ($log, MealFactory) {
+  .directive('mvMeal', function ($log, MealFactory, DayFactory) {
     return {
       templateUrl: 'views/templates/mvmeal.html',
       restrict: 'EAC',
@@ -9,12 +9,19 @@ angular.module('mensavoterFrontendApp')
         meal: '=',
         date: '='
       },
-      controller: function($scope) {
+      controller: function($scope, $element) {
 
         $scope.vote = function(mealID, voting) {
           MealFactory.vote($scope.date, mealID, voting)
-            .then(function(res) {
-              $log.log(res);
+            .then(function() {
+
+              if(voting === 'up') {
+                $element.find('.upvote i').addClass('mvfontgreen');
+              } else {
+                $element.find('.downvote i').addClass('mvfontred');
+              }
+
+              DayFactory.setMealVote(mealID, voting);
             }, function(err) {
               $log.error(err);
             });
--- app/scripts/services/DayFactory.js
+++ app/scripts/services/DayFactory.js
@@ -2,21 +2,46 @@
 
 angular.module('mensavoterFrontendApp')
   .factory('DayFactory', function (Restangular, $q) {
+
+    var day;
+
     return {
+      setDay: function(newDay) {
+        day = newDay;
+      },
+      getDay: function() {
+        return day;
+      },
+      setMealVote: function(mealID, vote) {
+        for(var i = 0; i < day.Mensa.meals.length; i++) {
+          if(day.Mensa.meals[i].id === mealID) {
+            if(vote === 'up') {
+              day.Mensa.meals[i].upvotes += 1;
+            } else {
+              day.Mensa.meals[i].downvotes += 1;
+            }
+            break;
+          }
+        }
+        this.calcScores();
+      },
+      calcScores: function() {
+        for(var i = 0; i < day.Mensa.meals.length; i++) {
+          day.Mensa.meals[i].score = day.Mensa.meals[i].upvotes-day.Mensa.meals[i].downvotes;
+        }
+      },
       getToday: function () {
         var deferred = $q.defer();
 
         Restangular.one('days', 'today').doGET()
           .then(function(res) {
 
-            // Sort by voting
-            for(var i = 0; i < res.Mensa.meals.length; i++) {
-              res.Mensa.meals[i].score = res.Mensa.meals[i].upvotes-res.Mensa.meals[i].downvotes;
-            }
+            this.setDay(res);
+            this.calcScores();
 
             deferred.resolve(res);
 
-          }, function(err) {
+          }.bind(this), function(err) {
             deferred.reject(err);
           });
 
